f71763d50445e0f7a715e8b4c85125122f80ac48
Ryan Pattison <ryan.m.pattison@gmail.com>
2013-10-10 11:59:14 -0400

    running code

--- feature_extraction/feature_extraction.py
+++ feature_extraction/feature_extraction.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-import os
+#import os
 import csv
 from argparse import ArgumentParser
 
@@ -26,20 +26,20 @@ def parse_args():
     parser.add_argument('columns', type=int, help="The number of 'cells' along the x-dimension in the image file's grid.")
     parser.add_argument('rows', type=int, help="The number of 'cells' along the y-dimension in the image file's grid.")
     parser.add_argument('filters', nargs='+', help="CSV files containing square matrices (3x3 or 5x5) to use as a convolution filter")
-    
+
     parser.add_argument('-o', help="output directory to write csv data to, default=stdout")
     parser.add_argument('-filtered_image', help="where to save the filtered output image, (default=no save file)")
-    
+
     args = parser.parse_args()
-    
+
     if args.columns <= 0 or args.rows <= 0:
         raise ValueError("rows and columns must be positive integers")
-    
+
     image_name = args.image
     args.image = Image.open(image_name)
-    
+
     # TODO: construct an output filename, filtered_image name,  if a directory is given ...
-    
+
     return args
 
 
@@ -68,7 +68,7 @@ def apply_filters(image, filters):
     return image
 
 
-def bounding_box(x, y, w, h, inset=0)
+def bounding_box(x, y, w, h, inset=0):
     return [x + inset, y + inset, x + w - 2 * inset, y + h - 2 * inset]
 
 
@@ -81,29 +81,28 @@ def analyze(image):
 def main(image, rows, columns, filter_list, csv_output=sys.stdout):
     cell_w = image.size[0] / columns
     cell_h = image.size[1] / rows
-    print image
-    print filters
-    print "Rows:\t", rows3 s
+    print "Rows:\t", rows
     print "Cols:\t", columns
     print "Cell Width:\t", cell_w
     print "Cell Height:\t", cell_h
     filtered_image = apply_filters(image, filter_list)
 
-    stats_data = [list() * rows]
+    stats_data = []
     for i in range(columns):
+        stats_data.append(list())
         for j in range(rows):
             bb = bounding_box(i * cell_w, j * cell_h, cell_w, cell_h)
-            cell = image.crop(bb)
-            stats_data[i][j] = analyze(cell)
+            cell = filtered_image.crop(bb)
+            stats_data[i].append(analyze(cell))
 
-    header_names = ['i', 'j'].extend(stats_data[0].keys())
+    header_names = stats_data[0][0].keys()
     stats_writer = csv.writer(csv_output)  #  maybe DictWriter would be nicer here.
-    stats_writer.writerow(header_names)
+    stats_writer.writerow(['i', 'j'] + header_names)
     for i in range(columns):
         for j in range(rows):
             row = [i, j]
             for k in header_names:
-                row.append(data[i][j][k])
+                row.append(stats_data[i][j][k])
             stats_writer.writerow(row)
 
 
@@ -112,8 +111,9 @@ if __name__ == "__main__":
         args = parse_args()
         image = args.image
         filters = read_filters(args.filters)
-        main(args.image, args.rows, args.columns, filters)
     except Exception as e:
         print e
         sys.exit(1)
+
+    main(args.image, args.rows, args.columns, filters)
 
