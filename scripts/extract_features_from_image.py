34ca7baca9a9779542d262348fa447b28e1c39b9
Douglas Anderson <dougjamesanderson@gmail.com>
2013-10-19 16:13:10 -0400

    Extract features script is now much nicer. Extracts keys quickly in a single thread

--- scripts/extract_features_from_image.py
+++ scripts/extract_features_from_image.py
@@ -12,21 +12,33 @@ def parse_args():
     parser.add_argument("width", type=int, help="The width of the map in cell")
     parser.add_argument("height", type=int, help="The height of the map in cell")
     parser.add_argument("intended", type=str, help="A csv file of the intend representation")
-    parser.add_argument("--keeptmp", dest="keeptmp", action="store_true", default=False, help="A flag to control removal of intermediate files")
-    # TODO add more args
-    # TODO add blur
-    # TODO add adjustable output
-    # TODO add add filter list
+
+    parser.add_argument('--output', help="""
+        output file or directory to write csv data to.
+        If directory file will be written in the form of: <dir name>/<image name>.csv
+        default=<current working directory>/<image_name>.csv""")
+    parser.add_argument('--tmpdir', default="tmp/", help="""
+        output file or directory to write intermediate csv data to: default=tmp/""")
+    parser.add_argument("--keeptmp", dest="keeptmp", action="store_true", default=False, help="""
+        Remove intermediate csv files. default=false""")
+
+    parser.add_argument("--noblur", dest="blur", action="store_false", default=True, help="""
+        Apply blur filter to cell images. default=true""")
+    parser.add_argument("--onlyblur", dest="onlyblur", action="store_true", default=False, help="""
+        Do not collect results for unfilted images. default=false""")
+    parser.add_argument("--permlen", type=int, default=2, help="""
+        The permuation length of the filters. default=2""")
+
+    args = parser.parse_args()
     if os.path.basename(os.getcwd()) != "handDrawnMapRecognition":
         raise Exception("Must be in root directory of the project")
-    if os.path.exists(os.path.join(os.getcwd(),"tmp")):
+    if os.path.exists(os.path.join(os.getcwd(),args.tmpdir)):
         raise Exception("Please clean up tmp dir. Exiting")
-    return(parser.parse_args())
-
-f_dir = "feature_extraction/filters/"
-out_dir = "tmp/"
-
-filters = ["horz", "vert", "dia1", "dia2"]
+    if not args.output:
+        args.output = os.path.join(os.getcwd(), os.path.basename(args.image).split(".")[0]+".csv")
+    elif os.path.isdir(args.output):
+        args.output = os.path.join(args.output, os.path.basename(args.image).split(".")[0]+".csv")
+    return(args)
 
 def feature_extraction(args):
     call(["python", "feature_extraction/feature_extraction.py"] + args)
@@ -39,29 +51,49 @@ def csv_join(files, output):
     call(["python", "scripts/csv_join.py"]+files, stdout=w)
     w.close()
 
-def main(image, width, height, intended, keeptmp):
-    os.mkdir(out_dir)
+def get_filter_lists(permlen, blur, onlyblur):
+    """
+    Creates a list of filters be be applied s.t. main can simply loop though them.
+    """
+    filter_dir = "feature_extraction/filters/"
+    idenity_filter = "iden"
+    filters = ["horz", "vert", "dia1", "dia2"]
+    blur_path = os.path.join(filter_dir,"lblur.csv")
+    lists = []
+    # Apply the idenity filter
+    if not onlyblur:
+        lists.append([os.path.join(filter_dir,idenity_filter+".csv")])
+    if blur or onlyblur:
+        lists.append([blur_path]+[os.path.join(filter_dir,idenity_filter+".csv")])
+    # Apply all permutions of filters for perutation length r
+    for r in range(1,permlen+1):
+        for perm in permutations(filters,r):
+            if not onlyblur:
+                lists.append([os.path.join(filter_dir,f+".csv") for f in perm])
+            if blur or onlyblur:
+                lists.append([blur_path]+[os.path.join(filter_dir,f+".csv") for f in perm])
+    return lists
 
-    for f in filters:
-        print "Extracting features under: ", f
-        feature_extraction([image, width, height,f_dir+f+".csv", "-output", out_dir])
-    for combo in permutations(filters,2):
-        print "Extracting features under: ", " and ".join(combo)
-        feature_extraction([image, width, height,f_dir+combo[0]+".csv",f_dir+combo[1]+".csv","-output", out_dir])
+def main(image, width, height, intended, output, tmpdir, keeptmp, blur, onlyblur, permlen):
+    os.mkdir(tmpdir)
+
+    for filter_list in get_filter_lists(permlen, blur, onlyblur):
+        print "Extracting features under:", " and ".join([os.path.basename(f).split(".")[0] for f in filter_list])
+        feature_extraction([image, width, height]+filter_list+["-output", tmpdir])
 
     print "Transforming intended csv"
-    map_pivot([intended, out_dir+"intended.csv"])
+    map_pivot([intended, tmpdir+"intended.csv"])
 
     print "Joining all intermediate csv files"
-    csv_files = [out_dir+csv_file for csv_file in os.listdir(out_dir)]
-    final_output = os.path.join(os.getcwd(), os.path.basename(image).split(".")[0]+".csv")
-    csv_join(csv_files, final_output)
+    csv_files = [tmpdir+csv_file for csv_file in os.listdir(tmpdir)]
+    csv_join(csv_files, output)
 
     if not keeptmp:
         print "Removing intermediate csv files"
-        rmtree(os.path.join(os.getcwd(),out_dir))
+        rmtree(os.path.join(os.getcwd(),tmpdir))
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.image, str(args.width), str(args.height), args.intended, args.keeptmp)
+    main(args.image, str(args.width), str(args.height), args.intended, args.output,\
+        args.tmpdir, args.keeptmp, args.blur, args.onlyblur, args.permlen)
 
