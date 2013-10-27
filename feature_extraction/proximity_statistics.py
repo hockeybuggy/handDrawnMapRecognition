6fef5a3ce06c5b880bff77c8ef266a1b5d40df4b
Douglas Anderson <dougjamesanderson@gmail.com>
2013-10-27 17:25:03 -0400

    Cleaned up quite a bit of my code for proximity statistics.

--- feature_extraction/proximity_statistics.py
+++ feature_extraction/proximity_statistics.py
@@ -15,19 +15,18 @@ def parse_args():
 
     parser.add_argument('map_csv', help="A csv file representing a map.")
     parser.add_argument('-output', help="output file or directory to write csv data to, default=stdout")
-    parser.add_argument('-null_class', default="NULL", help="The name of the null class, default=NULL")
+    parser.add_argument('-edge_class', default="EDGE", help="The name of the edge class, default=EDGE")
     parser.add_argument('-min_n_size', type=int, default=15, help="""
-        The minimum number of examples to be statistically valid, default=15
-    """)
-    parser.add_argument('-map_source', default="intended", help="""
-        The source of the input csv file, default='intended'
-    """)
+        The minimum number of examples to be statistically valid, default=15 """)
+    parser.add_argument('-map_classifcation_type', default="intended", help="""
+        The method of creation of the input csv file. Either 'intended' or
+        'classifier' reccomended. default='intended' """)
 
     args = parser.parse_args()
     return(args)
 
 
-def get_neighbours(x, y, map_list, directions, null_class):
+def get_neighbours(x, y, map_list, directions, edge_class):
     neighbours = dict()
     for direction in directions.keys():
         try:
@@ -36,7 +35,7 @@ def get_neighbours(x, y, map_list, directions, null_class):
                 raise IndexError() # do not allow negative indexing
             neighbours[direction] = get_class(neighbour_pair[0], neighbour_pair[1], map_list)
         except IndexError:
-            neighbours[direction] = null_class # Null class for edges
+            neighbours[direction] = edge_class # If it's out of bounds assign edge class
     return neighbours
 
 
@@ -57,6 +56,7 @@ def add_new_class_to_proximity_stats(prox_stats, new_class, directions):
             prox_stats[new_class][direction][n_class] = 0
     return prox_stats
 
+
 def get_dict_sum(stats, c_class, direction, n_class=None):
     ttl = 0.0 # Total number of 
     if n_class is None:
@@ -66,57 +66,74 @@ def get_dict_sum(stats, c_class, direction, n_class=None):
         ttl = stats[c_class][direction][n_class]
     return ttl
 
-def main(map_csv, out_fd, null_class, min_n_size, map_source):
-    r = csv.reader(open(map_csv, "r"))
-    w = csv.writer(out_fd)
-    directions = {'north':(0,1),'east':(1,0),'south':(0,-1),'west':(-1,0)}
 
-    map_list = []
-    for row in r:
-        map_list.append(row)
-
-    proximity_stats = dict() # [cell_class][direction][neighbour_class]
-    add_new_class_to_proximity_stats(proximity_stats, null_class, directions) # Add null class
+def get_proximity_counts(map_list, edge_class, directions): 
+    """ Returns dictionary in the form of:
+          [cell_class][direction][neighbour_class] = count of occurrences"""
+    proximity_stats = dict()
+    add_new_class_to_proximity_stats(proximity_stats, edge_class, directions) # Add null class
     for  i in range(0, len(map_list)):
         for j in range(0,len(map_list[i])):
             cell_class = get_class(j, i, map_list)
             if cell_class not in proximity_stats: # class has not been encountered yet.
                 add_new_class_to_proximity_stats(proximity_stats, cell_class, directions)
-            neighbours = get_neighbours(j, i, map_list, directions, null_class)
+            neighbours = get_neighbours(j, i, map_list, directions, edge_class)
             for direction in neighbours:
                 n_class = neighbours[direction]
                 if n_class not in proximity_stats[cell_class][direction]:
                     add_new_class_to_proximity_stats(proximity_stats, n_class, directions)
                 proximity_stats[cell_class][direction][n_class] += 1
+    return proximity_stats
 
-    proximity_prob = dict()
-    for c_class in proximity_stats.keys():
-        if c_class != null_class:
-            proximity_prob[c_class] = dict()
-            for direction in proximity_stats[c_class]:
-                proximity_prob[c_class][direction] = dict()
-                n_class_ttl = get_dict_sum(proximity_stats, c_class, direction)
-                for n_class in proximity_stats[c_class][direction]:
-                    n_class_sum = get_dict_sum(proximity_stats, c_class, direction, n_class)
+
+def calculate_probabilities(prox_counts, min_n_size, edge_class, map_classifcation_type):
+    """ Returns a dictionary in the form of: 
+            [cell_class][given_probability_of] = prob if a class in a direction given cell_class """
+    prox_prob = dict()
+    for c_class in prox_counts.keys():
+        if c_class != edge_class:
+            prox_prob[c_class] = dict()
+            for direction in prox_counts[c_class]:
+                prox_prob[c_class][direction] = dict()
+                n_class_ttl = get_dict_sum(prox_counts, c_class, direction)
+                for n_class in prox_counts[c_class][direction]:
+                    n_class_sum = get_dict_sum(prox_counts, c_class, direction, n_class)
                     if n_class_ttl >= min_n_size:
-                        proximity_prob[c_class][direction][n_class] = n_class_sum / n_class_ttl
+                        prox_prob[c_class][direction][n_class] = n_class_sum / n_class_ttl
                     else:
-                        proximity_prob[c_class][direction][n_class] = None
-
+                        prox_prob[c_class][direction][n_class] = None # Not enough examples to be valid
+    # Convert to a prettier format
+    key_name = "prob_of_{}_{}_given_{}"
     prob_headers = dict()
-    for c_class in proximity_prob.keys():
+    for c_class in prox_prob.keys():
         prob_headers[c_class] = dict()
-        for direction in proximity_prob[c_class].keys():
-            for n_class in proximity_prob[c_class][direction].keys():
-                given_prob = proximity_prob[c_class][direction][n_class]
-                prob_headers[c_class]["prob_of_"+n_class+"_"+direction+"_given_"+map_source] = given_prob
+        for direction in prox_prob[c_class].keys():
+            for n_class in prox_prob[c_class][direction].keys():
+                given_prob = prox_prob[c_class][direction][n_class]
+                prob_headers[c_class][key_name.format(n_class, direction, map_classifcation_type)] = given_prob
+    return prob_headers
 
-    header_names = ["i", "j", map_source] + prob_headers.get(prob_headers.keys()[0]).keys()
-    w.writerow(header_names)
+
+def get_header_from_prox_prob(prox_prob):
+    return prox_prob.get(prox_prob.keys()[0]).keys()
+
+
+def main(map_csv, out_fd, edge_class, min_n_size, map_classifcation_type):
+    reader = csv.reader(open(map_csv, "r"))
+    writer = csv.writer(out_fd)
+    directions = {'north':(0,1),'east':(1,0),'south':(0,-1),'west':(-1,0)}
+
+    map_list = [ row for row in reader] # read file into array of arrays
+
+    proximity_counts = get_proximity_counts(map_list, edge_class, directions)
+    proximity_prob = calculate_probabilities(proximity_counts, min_n_size, edge_class, map_classifcation_type)
+
+    header_names = ["i", "j", map_classifcation_type] + get_header_from_prox_prob(proximity_prob)
+    writer.writerow(header_names)
     for  i in range(0, len(map_list)):
         for j in range(0,len(map_list[i])):
             cell_class = get_class(j, i, map_list)
-            w.writerow([i, j, cell_class]+prob_headers[cell_class].values())
+            writer.writerow([i, j, cell_class]+proximity_prob[cell_class].values())
 
 
 if __name__ == "__main__":
@@ -125,4 +142,4 @@ if __name__ == "__main__":
         out_fd = open(args.output, "w")
     else:
         out_fd = sys.stdout
-    main(args.map_csv, out_fd, args.null_class, args.min_n_size, args.map_source)
+    main(args.map_csv, out_fd, args.edge_class, args.min_n_size, args.map_classifcation_type)
