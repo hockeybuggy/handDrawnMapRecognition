b7036926ce3d28a839e1c49867bcd6d4c983d8ed
Ryan Pattison <ryan.m.pattison@gmail.com>
2013-10-28 16:17:33 -0400

    added color search and replace script to remove grid lines

--- feature_extraction/greyscale.py
+++ feature_extraction/greyscale.py
@@ -2,6 +2,7 @@
 
 from argparse import ArgumentParser
 
+import ImageOps
 import Image
 
 
@@ -9,8 +10,8 @@ def parse_args():
     parser = ArgumentParser()
     parser.add_argument('input')
     parser.add_argument('output')
+    parser.add_argument('--auto_contrast', action="store_true")
     return parser.parse_args()
-
 
 def as_greyscale(image):
     if image.getbands() == ('L', ):
@@ -21,4 +22,7 @@ def as_greyscale(image):
 
 if __name__ == "__main__":
     options = parse_args()
-    as_greyscale(Image.open(options.input)).save(options.output)
+    image = Image.open(options.input)
+    if options.auto_contrast:
+        image = ImageOps.autocontrast(image)
+    as_greyscale(image).save(options.output)
