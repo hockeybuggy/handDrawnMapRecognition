5b4eb1545bb6b4fb19469a10dde00bb10333887e
Douglas Anderson <dougjamesanderson@gmail.com>
2013-11-17 12:27:14 -0500

    Added a new script to join named csv by the keys i,j,and map

--- feature_extraction/add_map_name.sh
+++ feature_extraction/add_map_name.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-head -n 1 $1 | sed 's/^/map,/' > $2
-tail -n +2 $1 | sed 's/^/'$3',/' >> $2
+TEMP=intermid.csv
+
+head -n 1 $1 | sed 's/^/map,/' > $TEMP
+tail -n +2 $1 | sed 's/^/'$3',/' >> $TEMP
+
+sed 's/^\([^,]*\),\([^,]*\),\([^,]*\),\(.*\)/\2,\3,\1,\4/g' $TEMP > $2
