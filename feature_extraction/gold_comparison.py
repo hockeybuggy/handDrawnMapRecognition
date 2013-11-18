2dfbe50644bdb69daa29aaef403c75e75d4602bf
Ryan Pattison <ryan.m.pattison@gmail.com>
2013-11-17 19:25:40 -0500

    gold script

---
+++ feature_extraction/gold_comparison.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import argparse
+import csv
+
+import Image
+import simpleyaml as yaml
+
+from compare_images import compare_images
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('classify_this', help='grid to classify')
+    parser.add_argument('rows', type=int, help='the number of rows in the grid')
+    parser.add_argument('cols', type=int, help='the number of columns in the grid')
+    parser.add_argument('gold', help='a yaml file of class: mean_image')
+    parser.add_argument('output_csv', help='best guess at intended csv')
+    parser.add_argument('-intended_csv', help='compare against these')
+    return parser.parse_args()
+
+
+def gold_classify(gold_images, cell_image):
+    return min((compare_images(gold, cell_image), className)
+        for className, gold in gold_images.items())[1]
+
+
+def bounding_box(x, y, w, h):
+    return (x, y, x + w, y + h)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    grid = Image.open(args.grid)
+    cell_w = grid.size()[0] / args.cols
+    cell_h = grid.size()[1] / args.rows
+    answer = []
+    
+    gold = yaml.load(open(args.gold))
+    for name, mean_img in gold.items():
+        gold[name] = Image.open(mean_img)
+    
+    output = [gold_classify(gold,
+                grid.crop(bounding_box(
+                    i * cell_w, j * cell_h, cell_w, cell_h))))
+                    for j in range(args.cols) for i in range(args.rows)]
+    
+    with open(args.output_csv, 'w') as out:
+        write = csv.writer(out)
+        for row in output:
+            write.writerow(row)
+    
+    if args.intended:
+        diff = 0
+        with open(args.intended) as f:
+            intended = csv.reader(f)
+            for answer, guess in zip(intended, output):
+                for a, b in zip(answer, guess):
+                    if a != b:
+                        diff += 1
+        print diff 
+            
+
